f1490d8ab94807e503858882286f67f98f6ac29a
jyjyjy <oqvxy5@gmail.com>
2021-07-21 17:02:21 +0900

    javascript chrome application project javascript array[] means make new space so recommend array.indexof() [out of index] no error

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "nice";
+console.log(myName);
+const amIFat = false;
+const amINull = null;
+
+const mon = "mon", tue = "tue";
+const daysOfWeek = [mon,tue,"wed","thu","fri","sat","sun"];
+const nonsense = [0,1,true,"right"];
+console.log(daysOfWeek, nonsense);
+console.log(daysOfWeek[4]);
